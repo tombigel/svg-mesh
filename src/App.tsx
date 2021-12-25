397149427728ce4dd0ffc5a2aaf71fae6d6171b7
Tom Bigelajzen <tombigel@wix.com>
2021-12-25 03:43:15 +0200

    colorful default

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,35 @@ import { Controls } from "./components/Controls";
 import { Stage } from "./components/Stage";
 
 function App() {
-  const [points, setPoints] = React.useState([
-    { x: 200, y: 200, color: "#FF0000" },
-  ]);
+  const [points, setPoints] = React.useState(
+    [
+      {
+          "x": 279.7303771972656,
+          "y": 241.024658203125,
+          "color": "#FF0000"
+      },
+      {
+          "x": 122.05767822265625,
+          "y": 85.09025573730469,
+          "color": "#4400ff"
+      },
+      {
+          "x": 183.1702880859375,
+          "y": 285.7411804199219,
+          "color": "#000000"
+      },
+      {
+          "x": 79.20132446289062,
+          "y": 226.319580078125,
+          "color": "#00ff88"
+      },
+      {
+          "x": 288.25323486328125,
+          "y": 120.594482421875,
+          "color": "#00e1ff"
+      }
+    ]
+  );
   const [selected, setSelected] = React.useState(0);
   const [stdDeviation, setStdDeviation] = React.useState(40);
   const [slope, setSlope] = React.useState(1);
